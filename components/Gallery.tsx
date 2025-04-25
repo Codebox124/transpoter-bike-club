af0b9cbd01ac3c0c119b8979a12c92b2a3728b29
Codebox124 <rajcodes733@gmail.com>
2025-04-25 13:27:54 +0100

    updatd

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -99,14 +99,7 @@ const Gallery = () => {
             </div>
           </div>
           
-          <div className="text-center mt-12">
-            <button className="px-8 py-3 bg-green-500 text-gray-900 font-semibold rounded-md hover:bg-green-400 transition duration-300 shadow-md inline-flex items-center">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
-                <path fillRule="evenodd" d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm12 12H4l4-8 3 6 2-4 3 6z" clipRule="evenodd" />
-              </svg>
-              View Full Gallery
-            </button>
-          </div>
+         
         </div>
       </section>
     );
